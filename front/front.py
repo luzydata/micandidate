c0a02e567d645ac28e0164f204804aa324cfba88
urdaibayc <urdaibayc@gmail.com>
2021-05-17 23:48:07 -0500

    Conected front to api query function.

--- front/front.py
+++ front/front.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from apiq import querry_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'my_secret_key'
@@ -11,7 +12,7 @@ app.config['SECRET_KEY']= 'my_secret_key'
 ##############################################################################
 
 class Consulta(FlaskForm):
-    consulta = StringField('Cual es la clave electoral', validators=[DataRequired()])
+    consulta = StringField('¿Qué area te gustaría consultar?', validators=[DataRequired()])
     submit = SubmitField('Informarse')
 
 
@@ -21,15 +22,22 @@ class Consulta(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print('Creando forma')
     form = Consulta()
+    print('Validando forma')
     if form.validate_on_submit():
         session['consulta'] = form.consulta.data
+        print('Valoda')
         return redirect(url_for('resutado'))
     return render_template('index.html', form=form)
 
 @app.route('/resutado')
 def resutado():
-    return render_template('resutado.html')
+    try:
+        output = querry_for()
+    except ServerError:
+        print('querry_for function error.')
+    return render_template('resutado.html', output=output)
 
 ##############################################################################
 ################## CONSULTA API ##############################################
